735a8512ed1bd935d5f8636cc29e87608162c1bc
chriskwong91 <chriskwong91@gmail.com>
2016-08-26 19:50:50 -0700

    Make link icons, sort links by visits, and filter links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,20 +1,15 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links, Auth, $location) {
-  // Your code here
-  $scope.data = {};
-  // Auth.signout();
-
   var auth = Auth.isAuth();
-  console.log(auth);
   if (!auth) {
     $location.path('/signin'); 
   }
 
+  $scope.data = {};
+
   Links.getAll().then(function(links) {
     $scope.data.links = links;
   });
-
-  $scope.isAuth = Auth.isAuth;
 
 });
